09e2e6aae8f7462cfcb10b1cd58552f664237327
novaeco <novatribolnovea@gmail.com>
2025-06-23 01:46:27 +0200

    Implement ST7262 LCD driver

--- components/drivers/lcd_st7262/lcd_st7262.c
+++ components/drivers/lcd_st7262/lcd_st7262.c
@@ -1,1 +1,37 @@
-// LCD ST7262 init
+// Minimal stub implementation of an ST7262 LCD driver
+
+#include "lcd_st7262.h"
+#include "core/utils/logging.h"
+
+static lv_disp_draw_buf_t s_draw_buf;
+static lv_color_t s_buf1[LV_HOR_RES_MAX * 10];
+static lv_color_t s_buf2[LV_HOR_RES_MAX * 10];
+
+// Flush callback used by LVGL. A real implementation would transmit the
+// pixel data contained in `color_p` for the area defined by `area`.
+static void st7262_flush(lv_disp_drv_t *drv, const lv_area_t *area, lv_color_t *color_p)
+{
+    (void)area;
+    (void)color_p;
+
+    // Signal to LVGL that flushing is finished
+    lv_disp_flush_ready(drv);
+}
+
+// Initialise the LCD controller and register the LVGL driver
+void lcd_st7262_init(void)
+{
+    lv_disp_draw_buf_init(&s_draw_buf, s_buf1, s_buf2, LV_HOR_RES_MAX * 10);
+
+    static lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.flush_cb = st7262_flush;
+    disp_drv.draw_buf = &s_draw_buf;
+    disp_drv.hor_res = 480;
+    disp_drv.ver_res = 320;
+
+    lv_disp_drv_register(&disp_drv);
+
+    log_info("ST7262", "display driver initialised");
+}
+
--- main/main.c
+++ main/main.c
@@ -1,8 +1,10 @@
 #include "core/animals/animals.h"
 #include "storage/storage.h"
+#include "drivers/lcd_st7262/lcd_st7262.h"
 
 void app_main(void)
 {
     animals_init();
     storage_init();
+    lcd_st7262_init();
 }
