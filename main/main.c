c32e7bedd856e19c2231539efdb5b0d260cf74e4
novaeco <novatribolnovea@gmail.com>
2025-06-23 01:55:31 +0200

    Add GT911 touch driver

--- main/main.c
+++ main/main.c
@@ -1,10 +1,12 @@
 #include "core/animals/animals.h"
 #include "storage/storage.h"
 #include "drivers/lcd_st7262/lcd_st7262.h"
+#include "drivers/touch_gt911/gt911.h"
 
 void app_main(void)
 {
     animals_init();
     storage_init();
     lcd_st7262_init();
+    gt911_init();
 }
